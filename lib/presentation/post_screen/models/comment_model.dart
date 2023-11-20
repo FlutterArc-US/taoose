3e106d4ca83fb39b791a01c7fe42871b3c233135
Naveed <naveed.dev30@gmail.com>
2023-11-20 15:26:22 +0500

    Update

--- lib/presentation/post_screen/models/comment_model.dart
+++ lib/presentation/post_screen/models/comment_model.dart
@@ -1,9 +1,11 @@
 class CommentModel {
+  final String id;
   final String commentedBy;
   final String comment;
   final String time;
 
   CommentModel({
+    required this.id,
     required this.comment,
     required this.time,
     required this.commentedBy,
@@ -11,6 +13,7 @@ class CommentModel {
 
   static CommentModel fromJson(Map<String, dynamic> json) {
     return CommentModel(
+      id: json["id"],
       comment: json['comment'],
       time: json['time'],
       commentedBy: json['commentedBy'],
